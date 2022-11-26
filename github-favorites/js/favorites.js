9ad5ccb07a311caa5318fc1115161ffef8eaaa15
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-11-26 10:58:48 -0300

    JS para desenhar e excluir as linhas da tabela

--- github-favorites/js/favorites.js
+++ github-favorites/js/favorites.js
@@ -6,19 +6,15 @@ export class Favorites{
 
     load(){
 
-        this.entries = [{
-            login: 'maykBrito',
-            name: 'Mayk Brito',
-            public_repos: '76',
-            followers: '1233'
-        },
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+        
+    }
 
-        {
-            login: 'icaro',
-            name: 'Big Bobra',
-            public_repos: '23',
-            followers: '122'
-        }]
+    delete(user){
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
     }
 }
 
@@ -45,6 +41,16 @@ export class FavoritesView extends Favorites{
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
 
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                
+                if(isOk){
+                    this.delete(user)
+                }
+            }
+
+            
             this.tbody.append(row)
         })
         
