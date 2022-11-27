16f26bb30d59b0ea7c9a2330eba423f4adfb7499
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-11-26 21:06:38 -0300

    Pesquisa retornando dados através da api do github

--- github-favorites/js/favorites.js
+++ github-favorites/js/favorites.js
@@ -1,13 +1,33 @@
+export class GithubUser{
+    static search(username){
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint).then(data => data.json()).then(({login, name, public_repos, followers}) => ({
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+    }
+}
+
 export class Favorites{
     constructor(root){
         this.root = document.querySelector(root)
         this.load()
+
     }
 
     load(){
 
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
         
+    }
+
+    async add(username){
+        const user = await GithubUser.search(username)
+
+        console.log(user)
     }
 
     delete(user){
@@ -26,7 +46,19 @@ export class FavoritesView extends Favorites{
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onAdd()
 
+    }
+
+    onAdd(){
+        const addButton = this.root.querySelector('.search button')
+
+        addButton.onclick = () => {
+
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update(){
